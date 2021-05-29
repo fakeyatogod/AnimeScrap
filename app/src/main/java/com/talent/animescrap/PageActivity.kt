4142e75bb571904d15f47b20a7c28066e505af5d
fakeyatogod <fakeyatokami@gmail.com>
2021-05-30 01:47:56 +0530

    Get download links from gogo

--- app/src/main/java/com/talent/animescrap/PageActivity.kt
+++ app/src/main/java/com/talent/animescrap/PageActivity.kt
@@ -1,13 +1,16 @@
 package com.talent.animescrap
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.opengl.Visibility
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import android.view.View
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.AsyncTaskLoader
@@ -20,10 +23,13 @@ import org.jsoup.Jsoup
 class PageActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<AnimeDetails> {
 
     private var contentLink: String? = "null"
+
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page)
+
+
 
         supportLoaderManager.initLoader(56, null, this)
     }
@@ -36,13 +42,15 @@ class PageActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<AnimeDet
             println(url)
             val doc = Jsoup.connect(url).get()
             val animeContent = doc.getElementsByClass("p-10-t")
+            val animeEpContent = doc.getElementsByClass("box p-10 p-15 m-15-b anime-metadetails")
+                .select("div:nth-child(6)").select("span").text()
             val animeCover = doc.getElementsByClass("cover").attr("src")
             println(animeCover)
             val animeDetails = arrayListOf<String>()
             for (element in animeContent) {
                 animeDetails.add(element.text())
             }
-            return AnimeDetails(animeDetails[0], animeDetails[1], animeCover)
+            return AnimeDetails(animeDetails[0], animeDetails[1], animeCover, animeEpContent)
         }
 
         override fun onStartLoading() {
@@ -83,11 +91,12 @@ class PageActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<AnimeDet
         textView2.text = data.animeDesc
         Picasso.get().load(data.animeCover).error(R.drawable.ic_broken_image)
             .placeholder(R.drawable.pgi2).into(coverImage)
-        var progressBar = findViewById<ProgressBar>(R.id.progressbarInPage)
+        val progressBar = findViewById<ProgressBar>(R.id.progressbarInPage)
         progressBar.visibility = View.GONE
         textView.visibility = View.VISIBLE
         textView2.visibility = View.VISIBLE
         coverImage.visibility = View.VISIBLE
+        setupSpinner(data.animeEpisodes)
 
     }
 
@@ -100,4 +109,86 @@ class PageActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<AnimeDet
         finish()
         super.onBackPressed()
     }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupSpinner(num: String) {
+
+        val epList = arrayListOf<String>()
+
+        for (i in num.toInt() downTo 1) {
+            epList.add(i.toString())
+        }
+
+        val spinner = findViewById<Spinner>(R.id.episodeSpinner)
+        spinner.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(parent.context, parent.selectedItem.toString(), Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        val arrayAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item, epList
+        )
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = arrayAdapter
+
+        val episodeButtonForSpinner = findViewById<Button>(R.id.episodeButtonForSpinner)
+        episodeButtonForSpinner.setOnClickListener {
+            var watchLink = contentLink
+            watchLink = watchLink?.replace("anime", "watch")
+            val animeEpUrl = "https://yugenani.me${watchLink}${spinner.selectedItem}"
+            var streamLink = "null"
+
+            Thread {
+                try {
+                    val streamAniLink = Jsoup.connect(animeEpUrl)
+                        .get().getElementsByClass("anime-download").attr("href")
+
+                    val goGoStreamLink = streamAniLink.replace("https://streamani.net/streaming.php?id=","https://gogo-stream.com/download?id=")
+                    val linksHashMap = LinkedHashMap<String, String> ()
+
+                    for (i in Jsoup.connect(goGoStreamLink).get().getElementsByClass("dowload")){
+                        linksHashMap[i.getElementsByTag("a").text().toString().replace("Download","")] =
+                            i.getElementsByTag("a").attr("href").toString()
+
+                    }
+                    for (i in linksHashMap.keys){
+                        println("$i = ${linksHashMap.get(i)}")
+                    }
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }.start()
+
+        }
+    }
+
+    internal open class HtmlHandler {
+        @JavascriptInterface
+        fun handleHtml(html: String?) {
+            // scrape the content here
+            //println(html)
+        }
+    }
+
+    class MyJavaScriptInterface internal constructor(private val ctx: Context) {
+        var html: String? = null
+
+        @JavascriptInterface
+        fun showHTML(_html: String?) {
+            html = _html
+        }
+    }
+
+
 }
--- app/src/main/java/com/talent/animescrap/model/AnimeDetails.kt
+++ app/src/main/java/com/talent/animescrap/model/AnimeDetails.kt
@@ -3,5 +3,6 @@ package com.talent.animescrap.model
 data class AnimeDetails(
     val animeName: String,
     val animeDesc: String,
-    val animeCover: String
+    val animeCover: String,
+    val animeEpisodes: String
 )
