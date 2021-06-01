90624543156b6a38b32f832fea3c03b69a3c5144
fakeyatogod <fakeyatokami@gmail.com>
2021-06-02 01:05:40 +0530

    Add SearchView :)

--- app/src/main/java/com/talent/animescrap/MainBottomNav.kt
+++ app/src/main/java/com/talent/animescrap/MainBottomNav.kt
@@ -1,17 +1,29 @@
 package com.talent.animescrap
 
+import android.content.Context
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.talent.animescrap.adapter.RecyclerAdapter
 import com.talent.animescrap.databinding.ActivityMainBottomNavBinding
+import com.talent.animescrap.model.Photos
+import org.jsoup.Jsoup
+import java.util.*
+
 
 class MainBottomNav : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBottomNavBinding
+    lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,4 +45,77 @@ class MainBottomNav : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.topbar_menu, menu)
+        val search = menu?.findItem(R.id.search_nav)
+        searchView = search?.actionView as SearchView
+        searchView.queryHint = "Search Anime"
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+
+                val newText2 = newText.lowercase(Locale.ENGLISH)
+                if (newText.length >= 3) {
+                    supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main_bottom_nav)
+                        ?.let {
+                            supportFragmentManager.beginTransaction()
+                                .hide(it).commit()
+                        }
+                    findViewById<BottomNavigationView>(R.id.nav_view).visibility = View.GONE
+                    val recyclerView = binding.recyclerView2
+                    recyclerView.visibility = View.VISIBLE
+                    recyclerView.layoutManager = GridLayoutManager(this@MainBottomNav, 2)
+                    val searchUrl = "https://yugenani.me/search/?q=${newText2.replace(" ", "+")}"
+                    println(searchUrl)
+                    Thread {
+                        val picInfo = arrayListOf<Photos>()
+                        val doc = Jsoup.connect(searchUrl).get()
+                        val allInfo = doc.getElementsByClass("anime-meta")
+                        for (item in allInfo) {
+                            val itemImage = item.getElementsByTag("img").attr("data-src")
+                            val itemName = item.getElementsByClass("anime-name").text()
+                            val itemLink = item.attr("href")
+                            val picObject = Photos(itemName, itemImage, itemLink)
+                            picInfo.add(picObject)
+                        }
+
+                        runOnUiThread {
+                            recyclerView.adapter = RecyclerAdapter(this@MainBottomNav, picInfo)
+                            recyclerView.setHasFixedSize(true)
+                        }
+                    }.start()
+
+                } else {
+                    val recyclerView = binding.recyclerView2
+                    recyclerView.visibility = View.GONE
+                }
+                return false
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onBackPressed() {
+        if (!searchView.isIconified) {
+            searchView.isIconified = true
+            searchView.onActionViewCollapsed()
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main_bottom_nav)
+                ?.let {
+                    supportFragmentManager.beginTransaction()
+                        .show(it).commit()
+                }
+            findViewById<BottomNavigationView>(R.id.nav_view).visibility = View.VISIBLE
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
 }
