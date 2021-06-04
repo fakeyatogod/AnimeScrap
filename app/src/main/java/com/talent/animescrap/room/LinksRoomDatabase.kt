2af57557ff4cd6017bfdcbcb7ae8b2dbf7ca1ba5
fakeyatogod <fakeyatokami@gmail.com>
2021-06-04 17:52:33 +0530

    Change app icon foreground to red hehe
    +Reformat code

--- app/src/main/java/com/talent/animescrap/room/LinksRoomDatabase.kt
+++ app/src/main/java/com/talent/animescrap/room/LinksRoomDatabase.kt
@@ -3,7 +3,7 @@ package com.talent.animescrap.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [FavLinks::class],version = 1)
+@Database(entities = [FavLinks::class], version = 1)
 abstract class LinksRoomDatabase : RoomDatabase() {
-    abstract fun linkDao() : LinkDao
+    abstract fun linkDao(): LinkDao
 }
--- app/src/main/java/com/talent/animescrap/ui/trending/TrendingFragment.kt
+++ app/src/main/java/com/talent/animescrap/ui/trending/TrendingFragment.kt
@@ -5,9 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.talent.animescrap.adapter.RecyclerAdapter
 import com.talent.animescrap.databinding.FragmentTrendingBinding
