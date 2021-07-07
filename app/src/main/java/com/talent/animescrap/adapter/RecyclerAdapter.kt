a570e6c80c87ed8b09387187767141f109cf7fdb
fakeyatogod <fakeyatokami@gmail.com>
2021-07-07 17:35:53 +0530

    Fix error in progressBar
    E AndroidRuntime: Caused by: java.lang.IllegalArgumentException: The indicatorSize cannot be less than twice of the trackThickness.

--- app/src/main/java/com/talent/animescrap/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/talent/animescrap/adapter/RecyclerAdapter.kt
@@ -23,7 +23,7 @@ class RecyclerAdapter(val context: Context, private val itemList: ArrayList<Phot
         val itemImage: ImageView = itemView.findViewById(R.id.imageView)
         val cView: com.google.android.material.card.MaterialCardView =
             itemView.findViewById(R.id.cView)
-        val progressInCard = itemView.findViewById<CircularProgressIndicator>(R.id.progressInCard)
+        val progressInCard: CircularProgressIndicator = itemView.findViewById(R.id.progressInCard)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
