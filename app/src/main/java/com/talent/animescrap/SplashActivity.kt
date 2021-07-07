109bc35de1a9e7326b22cf684e96f9c10e29983f
fakeyatogod <fakeyatokami@gmail.com>
2021-07-07 16:23:47 +0530

    Remove shit

--- app/src/main/java/com/talent/animescrap/SplashActivity.kt
+++ app/src/main/java/com/talent/animescrap/SplashActivity.kt
@@ -2,39 +2,20 @@ package com.talent.animescrap
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import org.jsoup.Jsoup.connect
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        var isConnected = false
-        Thread {
-            try {
-                connect("https://www.google.com").get()
-                isConnected = true
-            } catch (e: Exception) {
-                runOnUiThread {
-                    Toast.makeText(
-                        this,
-                        "No Internet Connection, Connect to the Internet and Restart App",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-                finish()
-            }
-            runOnUiThread {
-                if (isConnected) {
-                    val intent = Intent(this, MainBottomNav::class.java)
-                    startActivity(intent)
-                    finish()
-                }
-            }
-        }.start()
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainBottomNav::class.java)
+            startActivity(intent)
+            finish()
+        }, 400)
 
     }
 }
