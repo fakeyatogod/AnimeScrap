73bf99b0237fa901834dfdbb4f6a16c52844255f
fakeyatogod <fakeyatokami@gmail.com>
2022-07-27 21:42:14 +0530

    Support Dynamic Color Theme

--- app/src/main/java/com/talent/animescrap/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/talent/animescrap/ui/activities/SplashActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.color.DynamicColors
 import com.talent.animescrap.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        DynamicColors.applyToActivitiesIfAvailable(application)
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
