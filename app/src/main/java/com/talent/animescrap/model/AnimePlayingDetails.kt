0e900b6c6d2379c01c47b26cc75c0280007f1129
fakeyatogod <fakeyatokami@gmail.com>
2023-03-05 10:04:01 +0530

    Don't obfuscate AnimePlayingDetails Model

--- app/src/main/java/com/talent/animescrap/model/AnimePlayingDetails.kt
+++ app/src/main/java/com/talent/animescrap/model/AnimePlayingDetails.kt
@@ -1,8 +1,10 @@
 package com.talent.animescrap.model
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.parcelize.Parcelize
 
+@Keep
 @Parcelize
 data class AnimePlayingDetails(
     val animeName: String,
