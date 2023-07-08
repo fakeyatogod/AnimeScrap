d3c453419d060aefddf5f6144652a755afea9012
fakeyatogod <fakeyatokami@gmail.com>
2023-07-08 19:45:18 +0530

    Fix Allanime url

--- app/src/main/java/com/talent/animescrap/animesources/AllAnimeSource.kt
+++ app/src/main/java/com/talent/animescrap/animesources/AllAnimeSource.kt
@@ -10,17 +10,17 @@ import org.jsoup.Jsoup
 
 class AllAnimeSource : AnimeSource {
 
-    private val mainUrl = "https://api.allanime.to"
-
+    private val mainAPIUrl = "https://api.allanime.day/api"
+    private val referer = "https://allanime.to/"
     override suspend fun animeDetails(contentLink: String): AnimeDetails =
         withContext(Dispatchers.IO) {
             val hash = "9d7439c90f203e534ca778c4901f9aa2d3ad42c06243ab2c5e6b79612af32028"
             val url =
-                "$mainUrl/allanimeapi?variables=%7B%22_id%22%3A%22${contentLink}%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22"+
+                "$mainAPIUrl?variables=%7B%22_id%22%3A%22${contentLink}%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22"+
                         """sha256Hash%22%3A%22$hash%22%7D%7D"""
 
             println(url)
-            val res = getJson(url)!!.asJsonObject
+            val res = getJson(url,mapOf("Referer" to referer))!!.asJsonObject
 
             val data = res.asJsonObject["data"].asJsonObject["show"].asJsonObject
             val animeCover = allAnimeImage(data["thumbnail"].asString)
@@ -50,7 +50,7 @@ class AllAnimeSource : AnimeSource {
         val hash = "06327bc10dd682e1ee7e07b6db9c16e9ad2fd56c1b769e47513128cd5c9fc77a"
 
         val url =
-            """$mainUrl/allanimeapi?variables=%7B%22search%22%3A%7B%22allowAdult%22%3Afalse%2C%22allowUnknown%22%3Afalse%2C%22query%22%3A%22${
+            """$mainAPIUrl?variables=%7B%22search%22%3A%7B%22allowAdult%22%3Afalse%2C%22allowUnknown%22%3Afalse%2C%22query%22%3A%22${
                 searchedText.replace(
                     "+",
                     "%20"
@@ -59,7 +59,7 @@ class AllAnimeSource : AnimeSource {
                     """sha256Hash%22%3A%22$hash%22%7D%7D"""
 
         val res =
-            getJson(url)!!.asJsonObject["data"].asJsonObject["shows"].asJsonObject["edges"].asJsonArray
+            getJson(url,mapOf("Referer" to referer))!!.asJsonObject["data"].asJsonObject["shows"].asJsonObject["edges"].asJsonArray
         for (json in res) {
             val name = json.asJsonObject["name"].asString
             val image = allAnimeImage(json.asJsonObject["thumbnail"].asString)
@@ -74,11 +74,11 @@ class AllAnimeSource : AnimeSource {
             val animeList = arrayListOf<SimpleAnime>()
             val hash = "06327bc10dd682e1ee7e07b6db9c16e9ad2fd56c1b769e47513128cd5c9fc77a"
             val url =
-                "$mainUrl/allanimeapi?variables=%7B%22search%22%3A%7B%22allowAdult%22%3Afalse%2C%22allowUnknown%22%3Afalse%2C%22isManga%22%3Afalse%7D%2C%22limit%22%3A26%2C%22page%22%3A1%2C%22translationType%22%3A%22sub%22%2C%22countryOrigin%22%3A%22ALL%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22"+
+                "$mainAPIUrl?variables=%7B%22search%22%3A%7B%22allowAdult%22%3Afalse%2C%22allowUnknown%22%3Afalse%2C%22isManga%22%3Afalse%7D%2C%22limit%22%3A26%2C%22page%22%3A1%2C%22translationType%22%3A%22sub%22%2C%22countryOrigin%22%3A%22ALL%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22"+
                         "sha256Hash%22%3A%22$hash%22%7D%7D"
             println(url)
             val res =
-                getJson(url)!!.asJsonObject["data"].asJsonObject["shows"].asJsonObject["edges"].asJsonArray
+                getJson(url,mapOf("Referer" to referer))!!.asJsonObject["data"].asJsonObject["shows"].asJsonObject["edges"].asJsonArray
             for (json in res) {
                 val name = json.asJsonObject["name"].asString
                 val image = allAnimeImage(json.asJsonObject["thumbnail"].asString)
@@ -92,10 +92,10 @@ class AllAnimeSource : AnimeSource {
         withContext(Dispatchers.IO) {
             val animeList = arrayListOf<SimpleAnime>()
             val hash = "1fc9651b0d4c3b9dfd2fa6e1d50b8f4d11ce37f988c23b8ee20f82159f7c1147"
-            val url = """$mainUrl/allanimeapi?variables=%7B%22type%22%3A%22anime%22%2C%22size%22%3A30%2C%22dateRange%22%3A7%2C%22page%22%3A1%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22"""+
+            val url = """$mainAPIUrl?variables=%7B%22type%22%3A%22anime%22%2C%22size%22%3A30%2C%22dateRange%22%3A7%2C%22page%22%3A1%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22"""+
                         """sha256Hash%22%3A%22$hash%22%7D%7D"""
             val res =
-                getJson(url)!!.asJsonObject["data"].asJsonObject["queryPopular"].asJsonObject["recommendations"].asJsonArray
+                getJson(url,mapOf("Referer" to referer))!!.asJsonObject["data"].asJsonObject["queryPopular"].asJsonObject["recommendations"].asJsonArray
             for (json in res) {
                 val animeCard = json.asJsonObject["anyCard"].asJsonObject
                 val name = animeCard["name"].asString
@@ -120,11 +120,11 @@ class AllAnimeSource : AnimeSource {
             println(type)
             val hash = "5f1a64b73793cc2234a389cf3a8f93ad82de7043017dd551f38f65b89daa65e0"
             val url =
-                """$mainUrl/allanimeapi?variables=%7B%22showId%22%3A%22$animeUrl%22%2C%22translationType%22%3A%22$type%22%2C%22episodeString%22%3A%22$animeEpCode%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22"""+
+                """$mainAPIUrl?variables=%7B%22showId%22%3A%22$animeUrl%22%2C%22translationType%22%3A%22$type%22%2C%22episodeString%22%3A%22$animeEpCode%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22"""+
                         """sha256Hash%22%3A%22$hash%22%7D%7D"""
             println(url)
             val res =
-                getJson(url)!!.asJsonObject["data"].asJsonObject["episode"].asJsonObject["sourceUrls"].asJsonArray
+                getJson(url,mapOf("Referer" to referer))!!.asJsonObject["data"].asJsonObject["episode"].asJsonObject["sourceUrls"].asJsonArray
             val sortedSources =
                 res.sortedBy { if (!it.asJsonObject["priority"].isJsonNull) it.asJsonObject["priority"].asDouble else 0.0 }
                     .reversed()
