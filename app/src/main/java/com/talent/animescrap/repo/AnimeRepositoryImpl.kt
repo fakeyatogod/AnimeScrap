599bcd3036b0a426a7a834452aaacb115be5709b
fakeyatogod <fakeyatokami@gmail.com>
2023-07-20 00:34:04 +0530

    Add AsianHDPlay Source

--- app/src/main/java/com/talent/animescrap/repo/AnimeRepositoryImpl.kt
+++ app/src/main/java/com/talent/animescrap/repo/AnimeRepositoryImpl.kt
@@ -72,6 +72,9 @@ class AnimeRepositoryImpl @Inject constructor(
             "marin_moe" -> {
                 MarinMoeSource()
             }
+            "asian_hd_play" -> {
+                AsianHDPlaySource()
+            }
             else -> {
                 YugenSource()
             }
